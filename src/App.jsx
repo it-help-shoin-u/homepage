d6a8f9edb90bff19f282ac76fdbaf675a095f233
H.Tatsuhiro <admin@h-tatsu.com>
2023-06-09 13:53:07 +0900

    reactのセッティング

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <p>react</p>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import ReactDOM, {createRoot} from 'react-dom';
+import App from './App';
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
 alert("Hello");
