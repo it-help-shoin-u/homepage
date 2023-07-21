5efbea4632fa56ea741fac9b7f4ca7cda89a2a06
H.Tatsuhiro <admin@h-tatsu.com>
2023-07-21 11:48:00 +0900

    Fix SideMenu.jsx.

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -4,13 +4,14 @@ import { useNavigate } from "react-router-dom";
 
 function SideMenu() {
     const navigate = useNavigate();
+    const rootPath = "/homepage/";
     return (
         <div css={SideMenuStyle}>
-            <p onClick={() => navigate("/")}>トップページ</p>
-            <p onClick={() => navigate("hours_place")}>開室時間・場所</p>
-            <p onClick={() => navigate("details")}>対応可能な内容</p>
-            <p onClick={() => navigate("people")}>担当学生スタッフ</p>
-            <p onClick={() => navigate("contact")}>お問い合わせ</p>
+            <p onClick={() => navigate(rootPath)}>トップページ</p>
+            <p onClick={() => navigate(rootPath + "hours_place")}>開室時間・場所</p>
+            <p onClick={() => navigate(rootPath + "details")}>対応可能な内容</p>
+            <p onClick={() => navigate(rootPath + "people")}>担当学生スタッフ</p>
+            <p onClick={() => navigate(rootPath + "contact")}>お問い合わせ</p>
         </div>
     );
 }
